f6b76e069e5aceee8ddd4487e1ea51d49fccbe0e
Jyoti-kiran03 <143993446+Jyoti-kiran03@users.noreply.github.com>
2026-02-14 22:02:30 +0530

    Update message.js

--- message.js
+++ message.js
@@ -1,10 +1,38 @@
+const main = "You are the most beautiful part of my life 💖";
+const sub = "And from today, it's always you & me 💞";
+
+let i = 0, j = 0;
+
+function typeMain() {
+  if (i < main.length) {
+    document.getElementById("msg").innerHTML += main.charAt(i);
+    i++;
+    setTimeout(typeMain, 80);
+  } else {
+    setTimeout(typeSub, 500);
+  }
+}
+
+function typeSub() {
+  if (j < sub.length) {
+    document.getElementById("sub").innerHTML += sub.charAt(j);
+    j++;
+    setTimeout(typeSub, 60);
+  }
+}
+
+typeMain();
+
+// falling hearts
+const petals = document.querySelector(".petals");
+
 setInterval(() => {
   const heart = document.createElement("span");
   heart.innerHTML = "💖";
   heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = Math.random() * 35 + 15 + "px";
+  heart.style.fontSize = Math.random() * 30 + 15 + "px";
   heart.style.animationDuration = Math.random() * 3 + 4 + "s";
-  document.querySelector(".petals").appendChild(heart);
+  petals.appendChild(heart);
 
   setTimeout(() => heart.remove(), 7000);
 }, 400);
